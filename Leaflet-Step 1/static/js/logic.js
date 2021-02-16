40de50e060541a864a5302b34a50ff090317ed9b
Ronald Clarke <ronnie.clarke@utoronto.ca>
2021-02-16 17:58:19 -0500

    color change

--- Leaflet-Step 1/static/js/logic.js
+++ Leaflet-Step 1/static/js/logic.js
@@ -18,9 +18,29 @@ d3.json(geoData, function(data) {
   data.features.forEach(function (response) {
     L.circle([response.geometry.coordinates[1], response.geometry.coordinates[0]], {
       color: "none",
-      fillColor: "blue",
+      fillColor: colorDepth(response.geometry.coordinates[2]),
       fillOpacity: 0.5,
       radius: (response.properties.mag)*20000
     }).addTo(myMap)
   })
 })
+function colorDepth(depth) {
+  if (depth <=10) {
+    return "#66ff33"
+  }
+  else if (depth > 10 && depth < 30) {
+    return "#d5ff80"
+  }
+  else if (depth > 30 && depth < 50) {
+    return "#ffe680"
+  }
+  else if (depth > 50 && depth < 70) {
+    return "#ffb366"
+  }
+  else if (depth > 70 && depth < 90) {
+    return "#ff704d"
+  }
+  else {
+    return "#b32400"
+  }
+}
